2bed23a810aa4474f718a2276a1abfe5ee4b8287
deneick <d.eickhorn@mail.de>
2018-06-27 18:31:05 +0200

    .3

--- Masterarbeit/0cerr2.py
+++ Masterarbeit/0cerr2.py
@@ -4,4 +4,4 @@ set_log_levels(levels={'pymor': 'WARN'})
 if not os.path.exists("dats"):
 	os.makedirs("dats")
 
-cerr2D(1,15,6, 'dirichlet', "dats/cerr2D_bsp2_test.dat",rang = np.arange(-50.,50.,10.), plot = True, resolution = 50)
+cerr2D(10,15,6, 'dirichlet', "dats/cerr2D_bsp2.dat" resolution = 50)
