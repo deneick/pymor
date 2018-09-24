5e7ceefbc6f278a11529d3c90b1f3683fd43e2d6
Dennis Eickhorn <d_eick05@COBB.uni-muenster.de>
2018-09-24 11:00:01 +0200

    unglk

--- Masterarbeit/0unglk1.py
+++ Masterarbeit/0unglk1.py
@@ -1,3 +1,3 @@
 from evaluations import *
 
-ungleichungk(it= 10, n=15, boundary = 'robin', save = "dats/ungleichungk_bsp1.dat", cloc0 = 0, cloc1 = 0.02*(5-1j), cloc2 = 0.0016*(8-1j)):
+ungleichungk(it= 10, n=15, boundary = 'robin', save = "dats/ungleichungk_bsp1.dat", cloc0 = 0, cloc1 = 0.02*(5-1j), cloc2 = 0.0016*(8-1j))
