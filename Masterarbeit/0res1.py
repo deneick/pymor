45433ea86cab3e2123e89530a41a89c897748426
deneick <d.eickhorn@mail.de>
2018-09-29 21:43:34 +0200

    res

--- Masterarbeit/0res1.py
+++
@@ -1,3 +0,0 @@
-from evaluations import *
-
-resolution(10, 6, 15, 'robin', save = "dats/res1.dat", cloc = 1.-0.5j)
--- Masterarbeit/0res2.py
+++
@@ -1,3 +0,0 @@
-from evaluations import *
-
-resolution(10, 50, 15, 'robin', save = "dats/res2.dat", cloc = 37-5j)
