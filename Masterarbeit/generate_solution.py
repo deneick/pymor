6949a156bc48e97e48b714028047d7b586d4cb06
Dennis Eickhorn <d_eick05@COBB.uni-muenster.de>
2018-10-24 16:11:29 +0200

    csi3

--- Masterarbeit/generate_solution.py
+++ Masterarbeit/generate_solution.py
@@ -69,7 +69,7 @@ def create_bases(gq, lq, num_testvecs, transfer = 'dirichlet', testlimit = None,
 			#print "Maxnorm: ", maxnorm
 		#print "Basisgroesse: ", len(basis)
 		bases[space] = basis
-
+"""
 	#Fuer den Fall beta_tilde /= beta_h (vgl. S. 45):
 	print "inf-sup Konstante wird neu berechnet"
 	inf_sup_old = gq["inf_sup_constant"].copy()
@@ -111,6 +111,7 @@ def create_bases(gq, lq, num_testvecs, transfer = 'dirichlet', testlimit = None,
 				#print "Maxnorm: ", maxnorm
 			#print "Basisgroesse: ", len(basis)
 			bases[space] = basis
+"""
 	return bases
 
 #nicht-adaptive Basiserstellung (Algorithmus 4):
