a83ff2b47fe2e5383f54f8ae4b88600f6d86ae69
Dennis Eickhorn <d_eick05@COBB.uni-muenster.de>
2018-11-14 15:18:08 +0100

    knorm

--- Masterarbeit/nulls/0res30.4.py
+++
@@ -1,7 +0,0 @@
-from evaluations import *
-
-resolution(50, 0.1, 30, 'robin', save = "dats/res0.30.dat", cloc = 0)
-#resolution(50, 25, 30, 'robin', save = "dats/res25.30.dat", cloc = 0)
-#resolution(50, 50, 30, 'robin', save = "dats/res50.30.dat", cloc = 0)
-#resolution(50, 100, 30, 'robin', save = "dats/res100.30.dat", cloc = 0)
-#resolution(50, 200, 30, 'robin', save = "dats/res200.30.dat", cloc = 0)
