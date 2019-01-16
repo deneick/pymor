71c881be45a2c3cb5a9c3c187c5597eba0f982c8
Dennis Eickhorn <d_eick05@COBB.uni-muenster.de>
2019-01-16 12:05:47 +0100

    maxwell

--- Masterarbeit/nulls/0eva2.py
+++
@@ -1,3 +0,0 @@
-from evaluations import *
-
-evaluation(it = 100, lim = 80, k=6, boundary = 'dirichlet', save = "dats/eva_bsp2.dat", cloc=1.5)
--- Masterarbeit/nulls/0kerr1.py
+++
@@ -1,3 +0,0 @@
-from evaluations import *
-
-kerr(it = 10, n = 15, boundary = 'robin', save = "dats/k_err_bsp1.dat", cloc0 = 0, cloc1 = 0.02*(5-1j), cloc2 = 0.0016*(8-1j))
