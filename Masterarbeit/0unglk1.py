3ad33743474f4802a4fce9a7ad6f88da1fdf637b
Dennis <d.eickhorn@wwu.de>
2019-02-15 10:55:13 +0100

    unglk

--- Masterarbeit/0unglk1.py
+++ Masterarbeit/0unglk1.py
@@ -1,3 +1,6 @@
 from evaluations import *
 
-ungleichungk3(it= 10, acc=1e-2, boundary = 'robin', save = "dats/ungleichungk_bsp1_rs1.dat", cloc0 = 0, cloc1 = 0.02*(5-1j), cloc2 = 0.0016*(8-1j))
+#ungleichungk3(it= 10, acc=1e-2, boundary = 'robin', save = "dats/ungleichungk_bsp1_rs1.dat", cloc0 = 0, cloc1 = 0.02*(5-1j), cloc2 = 0.0016*(8-1j))
+
+ungleichungk3(it= 10, acc=1e-2, boundary= 'dirichlet', save = "dats/ungleichungk_bsp2_rs1.dat", cloc0 = 1, cloc1 = -0.03, cloc2 = 0.014)
+ungleichungk3(it= 10, acc=1e-2, boundary= 'neumann', save = "dats/ungleichungk_bsp3_rs1.dat", cloc0 =0, cloc1 = 0.2, cloc2 = 0.01)
